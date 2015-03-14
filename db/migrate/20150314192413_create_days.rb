463327109596d188d1f4bfc1293d8564724d136e
simacna <simacna@gmail.com>
2015-03-14 15:38:20 -0400

    referencing databases

--- db/migrate/20150314192413_create_days.rb
+++ db/migrate/20150314192413_create_days.rb
@@ -1,7 +1,7 @@
 class CreateDays < ActiveRecord::Migration
   def change
     create_table :days do |t|
-
+      t.integer :date	
       t.timestamps
     end
   end
--- db/migrate/20150314192446_create_events.rb
+++ db/migrate/20150314192446_create_events.rb
@@ -1,7 +1,10 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
-
+      t.string :name
+      t.datetime :from
+      t.datetime :to
+      t.references :day	
       t.timestamps
     end
   end
